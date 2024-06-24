0dc6a03b849f597b2b2172c626022e4cefe8d84d
sumitgit3 <sumitkumar703011@gmail.com>
2024-06-24 14:44:11 +0530

    connected delete  function to api and fixed addnote where it didn't added id to the note

--- src/Context/Notes/NoteState.js
+++ src/Context/Notes/NoteState.js
@@ -10,7 +10,7 @@ const NoteState = (props) => {
       method: "GET", // *GET, POST, PUT, DELETE, etc.
       mode: "cors", // no-cors, *cors, same-origin
       headers: {
-        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2NzQ1MTYyYzdkMWNjNDAwNjg3ZjhhMyIsImlhdCI6MTcxOTEzMjc3NCwiZXhwIjoxNzE5MTM2Mzc0fQ.MgpvOE_u2JXTuFcspn3t5QX_gtILid-4As0PhJEv_WM"
+        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2NzQ1MTYyYzdkMWNjNDAwNjg3ZjhhMyIsImlhdCI6MTcxOTIxNDYxNiwiZXhwIjoxNzE5MjM2MjE2fQ.oq1dz5Kzo8ZSVulz1I6eST2RHSKbi1SUycvVDX8Ey-Y"
       }
     });
     const res = await response.json();
@@ -26,20 +26,31 @@ const NoteState = (props) => {
       headers: {
         "Content-Type": "application/json",
         // 'Content-Type': 'application/x-www-form-urlencoded',
-        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2NzQ1MTYyYzdkMWNjNDAwNjg3ZjhhMyIsImlhdCI6MTcxOTEzMjc3NCwiZXhwIjoxNzE5MTM2Mzc0fQ.MgpvOE_u2JXTuFcspn3t5QX_gtILid-4As0PhJEv_WM"
+        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2NzQ1MTYyYzdkMWNjNDAwNjg3ZjhhMyIsImlhdCI6MTcxOTIxNDYxNiwiZXhwIjoxNzE5MjM2MjE2fQ.oq1dz5Kzo8ZSVulz1I6eST2RHSKbi1SUycvVDX8Ey-Y"
       },
       body: JSON.stringify({title,description,tag}), // body data type must match "Content-Type" header
     });
-    const res = await response.json(); // parses JSON response into native JavaScript objects
+    const noteInResponse = await response.json(); // parses JSON response into native JavaScript objects-> to get the new added note with id
     //concat don't modify original and dont use push to update notes array as state should be modified only be setNotes
-    if(response.ok) setNotes(notes.concat(note));
-    else console.log(res);
+    if(response.ok) setNotes(notes.concat(noteInResponse));
+    else console.log(noteInResponse);
 
   }
   //Delete a note
-  const deleteNote = (id) => {
-    let newNotes = notes.filter((e) => e._id !== id);
-    setNotes(newNotes);
+  const deleteNote = async (id) => {
+    const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+      method: "DELETE", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      headers: {
+        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2NzQ1MTYyYzdkMWNjNDAwNjg3ZjhhMyIsImlhdCI6MTcxOTIxNDYxNiwiZXhwIjoxNzE5MjM2MjE2fQ.oq1dz5Kzo8ZSVulz1I6eST2RHSKbi1SUycvVDX8Ey-Y"
+      }
+    });
+    const res = await response.json();
+    console.log(res);
+    if(response.ok) {
+      let newNotes = notes.filter((e) => e._id !== id);
+      setNotes(newNotes);
+    }
   }
   return (
     <>
